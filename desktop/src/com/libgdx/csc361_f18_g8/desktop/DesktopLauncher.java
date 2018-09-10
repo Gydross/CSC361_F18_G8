7eeb93f8d6ed73e1500383f49776287caac64ab8
Gydross <winkpso2.3@gmail.com>
2018-09-10 13:33:22 -0400

    Separated animation frames from static images

--- desktop/src/com/libgdx/csc361_f18_g8/desktop/DesktopLauncher.java
+++ desktop/src/com/libgdx/csc361_f18_g8/desktop/DesktopLauncher.java
@@ -2,11 +2,32 @@ package com.libgdx.csc361_f18_g8.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.libgdx.csc361_f18_g8.CSC361_F18_G8;
+import com.libgdx.csc361_f18_g8.CanyonBunnyMain;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher
+{
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+	
+	public static void main(String[] arg)
+	{
+		if (rebuildAtlas)
+		{
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "assets/images", "assets/images", "canyonbunny.pack");
+		}
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CSC361_F18_G8(), config);
+		config.title = "CanyonBunny";
+		config.width = 800;
+		config.height = 480;
+		
+		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
