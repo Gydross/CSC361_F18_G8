87787ed1d050c3cecb4da93864fe4e5fb322e64b
Connor <co0168@cs.ship.edu>
2018-09-16 17:23:42 -0400

    Rock graphics done

--- core/src/com/libgdx/csc361_f18_g8/game/objects/Rock.java
+++ core/src/com/libgdx/csc361_f18_g8/game/objects/Rock.java
@@ -39,7 +39,36 @@ public class Rock extends AbstractGameObject
 		setLength(length + amount);
 	}
 	
-	
+	@Override
+	public void render(SpriteBatch batch)
+	{
+		TextureRegion reg = null;
+		float relX = 0;
+		float relY = 0;
+		
+		//Draw left edge
+		reg = regEdge;
+		relX -= dimension.x / 4;
+		batch.draw(reg.getTexture(), position.x + relX, position.y + relY,
+				origin.x, origin.y, dimension.x/4, dimension.y, scale.x, scale.y, rotation
+				reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(), false, false);
+		//draw middle
+		relX = 0;
+		reg = regMiddle;
+		for (int i=0; i<length;i++)
+		{
+			batch.draw(reg.getTexture(), position.x + relX, position.y + relY,
+					origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation
+					reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(), false, false);
+			relX += dimension.x;
+		}
+		//draw right edge
+		reg = regEdge;
+		batch.draw(reg.getTexture(), position.x + relX, position.y + relY,
+				origin.x, origin.x + dimension.x/8, origin.y, dimension.x/4,
+				dimension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(),
+				reg.getRegionWidth(), reg.getRegionHeight(), true, false);
+	}
 }
 
 
