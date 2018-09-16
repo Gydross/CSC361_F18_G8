9837753f39b90c77b961b24d800bd5ad0c06957e
Connor <co0168@cs.ship.edu>
2018-09-16 12:36:56 -0400

    i swear if this doesnt go on the right branch

---
+++ core/src/com/libgdx/csc361_f18_g8/game/Assets.java
@@ -0,0 +1,13 @@
+package com.libgdx.csc361_f18_g8.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Disposable;
+import com.libgdx.csc361_f18_g8.util.Constants;
+
+public class Assets {
+
+}
