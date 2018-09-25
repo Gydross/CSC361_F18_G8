cd11ffccaeb692b6dfca34297ffdba56133561ac
Connor <co0168@cs.ship.edu>
2018-09-25 17:59:03 -0400

    began updates on WorldController

--- core/src/com/libgdx/csc361_f18_g8/game/WorldController.java
+++ core/src/com/libgdx/csc361_f18_g8/game/WorldController.java
@@ -109,6 +109,7 @@ public class WorldController extends InputAdapter
 	public void update(float deltaTime) 
 	{
 		handleDebugInput(deltaTime);
+		level.update(deltaTime);
 		cameraHelper.update(deltaTime);
 
 	}
