3983d1ec23a6c0850c60b47bf5bc2dfe6087fc79
Connor <co0168@cs.ship.edu>
2018-09-25 17:01:27 -0400

    Started GoldCoin Class

---
+++ core/src/com/libgdx/csc361_f18_g8/game/objects/GoldCoin.java
@@ -0,0 +1,16 @@
+package com.libgdx.csc361_f18_g8.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.libgdx.csc361_f18_g8.game.Assets;;
+
+/**
+ * Gold Coin class which extends off of
+ * AbstractGameObject
+ * @author Connor Orischak
+ *
+ */
+public class GoldCoin 
+{
+
+}
