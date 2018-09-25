3926f42c6f7b7050471a6da7d8f718a51517d0ea
Connor <co0168@cs.ship.edu>
2018-09-25 17:18:09 -0400

    started feather object

---
+++ core/src/com/libgdx/csc361_f18_g8/game/objects/Feather.java
@@ -0,0 +1,41 @@
+package com.libgdx.csc361_f18_g8.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.libgdx.csc361_f18_g8.game.Assets;
+/**
+ * Feather class that extends off of
+ * AbstractGameObject
+ * @author Connor Orischak
+ *
+ */
+public class Feather extends AbstractGameObject
+{
+	private TextureRegion regFeather;
+	public boolean collected;
+
+	public Feather ()
+	{
+		init();
+	}
+
+	private void init ()
+	{
+		dimension.set(0.5f, 0.5f);
+		regFeather = Assets.instance.feather.feather;
+		// Set bounding box for collision detection
+		bounds.set(0, 0, dimension.x, dimension.y);
+		collected = false;
+	}
+	
+	public void render (SpriteBatch batch) 
+	{
+		if (collected) return;
+		TextureRegion reg = null;
+		reg = regFeather;
+		batch.draw(reg.getTexture(), position.x, position.y,
+				origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y,
+				rotation, reg.getRegionX(), reg.getRegionY(),
+				reg.getRegionWidth(), reg.getRegionHeight(), false, false);
+	}
+}
