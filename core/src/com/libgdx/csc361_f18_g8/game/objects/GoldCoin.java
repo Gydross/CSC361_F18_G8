4940b3973a11409cdd6abd71d6f961bbcf77be21
Connor <co0168@cs.ship.edu>
2018-09-25 17:13:23 -0400

    fixed typo in gold coin

--- core/src/com/libgdx/csc361_f18_g8/game/objects/GoldCoin.java
+++ core/src/com/libgdx/csc361_f18_g8/game/objects/GoldCoin.java
@@ -2,7 +2,7 @@ package com.libgdx.csc361_f18_g8.game.objects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.libgdx.csc361_f18_g8.game.Assets;;
+import com.libgdx.csc361_f18_g8.game.Assets;
 
 /**
  * Gold Coin class which extends off of
