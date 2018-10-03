afea00155ae2f367cbd0c5177a21416bb1a02bce
connor <co0168@cs.ship.edu>
2018-10-03 13:24:12 -0400

    added physics and floating mechanics to rocks

--- core/src/com/libgdx/csc361_f18_g8/game/objects/Rock.java
+++ core/src/com/libgdx/csc361_f18_g8/game/objects/Rock.java
@@ -3,6 +3,8 @@ package com.libgdx.csc361_f18_g8.game.objects;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.libgdx.csc361_f18_g8.game.Assets;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Rock class object
@@ -14,6 +16,12 @@ public class Rock extends AbstractGameObject
 	private TextureRegion regEdge;
 	private TextureRegion regMiddle;
 	private int length;
+	// chapter 8 instance variables for floating rocks
+	private final float FLOAT_CYCLE_TIME = 2.0f;
+	private final float FLOAT_AMPLITUDE = 0.25f;
+	private float floatCycleTimeLeft;
+	private boolean floatingDownwards;
+	private Vector2 floatTargetPosition;
 
 	public Rock()
 	{
@@ -27,6 +35,11 @@ public class Rock extends AbstractGameObject
 		regMiddle = Assets.instance.rock.middle;
 		// start length of this rock
 		setLength(1);
+		// gives floating details to rocks (ch8)
+		floatingDownwards = false;
+		floatCycleTimeLeft = MathUtils.random(0,
+				FLOAT_CYCLE_TIME / 2);
+		floatTargetPosition = null;
 	}
 
 	public void setLength(int length)
@@ -70,6 +83,27 @@ public class Rock extends AbstractGameObject
 				origin.x + dimension.x/8, origin.y, dimension.x/4,
 				dimension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(),
 				reg.getRegionWidth(), reg.getRegionHeight(), true, false);
+	}
+
+	/**
+	 * updates the position of the rock
+	 * and makes the rock look like it is moving on its own
+	 */
+	@Override
+	public void update (float deltaTime)
+	{
+		super.update(deltaTime);
+		floatCycleTimeLeft -= deltaTime;
+		if (floatTargetPosition == null)
+			floatTargetPosition = new Vector2(position);
+		if (floatCycleTimeLeft<= 0) 
+		{
+			floatCycleTimeLeft = FLOAT_CYCLE_TIME;
+			floatingDownwards = !floatingDownwards;
+			floatTargetPosition.y += FLOAT_AMPLITUDE
+					* (floatingDownwards ? -1 : 1);
+		}
+		position.lerp(floatTargetPosition, deltaTime);
 	}
 }
 
